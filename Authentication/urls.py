31d3b79bd727a2b48e595df446ec369b2f62e466
Mimya2000 <rejuana.islam@northsouth.edu>
2022-04-13 00:13:19 +0600

    forget password link genarated, link sent to email

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
          views.activate, name='activate'),
     path('resend_link/<str:pk>/', views.resend_link, name='resend'),
+
 ]
