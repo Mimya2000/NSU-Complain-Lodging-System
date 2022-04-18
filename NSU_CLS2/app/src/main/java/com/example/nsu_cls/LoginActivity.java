19693ccfcff36122eeb58b8986a45fca9dd38b88
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2022-04-19 02:14:19 +0600

    login interface done

---
+++ NSU_CLS2/app/src/main/java/com/example/nsu_cls/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.nsu_cls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
